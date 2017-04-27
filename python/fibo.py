d305409dfbacabec3b6b070a1148fceb48bf6150
Bob Baxley <bob.baxley@gmail.com>
2017-04-27 18:53:37 -0400

    import future print

--- python/fibo.py
+++ python/fibo.py
@@ -3,7 +3,7 @@
 def fib(n):    # write Fibonacci series up to n
     a, b = 0, 1
     while b < n:
-        print b,
+        print(b, end=' ')
         a, b = b, a+b
 
 def fib2(n):   # return Fibonacci series up to n
