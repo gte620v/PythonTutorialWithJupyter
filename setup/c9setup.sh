ae6c417765ece5599df04068660a156a21f7acd0
Harrison Bond <baronvonbadguy@gmail.com>
2017-06-02 21:52:53 -0400

    fixed incorrect command

--- setup/c9setup.sh
+++ setup/c9setup.sh
@@ -9,6 +9,6 @@ rm Miniconda3-latest-Linux-x86_64.sh
 export PATH="$HOME/miniconda3/bin:$PATH"
 echo 'export PATH=$HOME/miniconda3/bin:$PATH' >> ~/.bashrc
 conda install jupyter notebook --yes
-makedir ~/.jupyter
+mkdir ~/.jupyter
 touch ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.token = u''" >> ~/.jupyter/jupyter_notebook_config.py
