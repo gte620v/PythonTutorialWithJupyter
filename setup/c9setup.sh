bfd801890b15f58f94bb8d8337d3df4312c78cbe
Harrison Bond <baronvonbadguy@gmail.com>
2017-06-02 21:12:14 -0400

    updated setup to install jupyter

--- setup/c9setup.sh
+++ setup/c9setup.sh
@@ -6,4 +6,5 @@ fi
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b
 export PATH="$HOME/miniconda3/bin:$PATH"
-echo 'export PATH=$HOME/miniconda3/bin:$PATH' >> ~/.bashrc && source ~/.bashrc
+echo 'export PATH=$HOME/miniconda3/bin:$PATH' >> ~/.bashrc
+conda install jupyter notebook -y
