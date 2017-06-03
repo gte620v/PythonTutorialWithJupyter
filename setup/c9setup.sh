c002362d685d0c3ca7aa27f9306caddd039f0df9
Harrison Bond <baronvonbadguy@gmail.com>
2017-06-02 21:35:10 -0400

    updated setup to configure notebook correctly

--- setup/c9setup.sh
+++ setup/c9setup.sh
@@ -5,6 +5,10 @@ then
 fi
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b
+rm Miniconda3-latest-Linux-x86_64.sh
 export PATH="$HOME/miniconda3/bin:$PATH"
 echo 'export PATH=$HOME/miniconda3/bin:$PATH' >> ~/.bashrc
 conda install jupyter notebook --yes
+makedir ~/.jupyter
+touch ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.token = u''" >> ~/.jupyter/jupyter_notebook_config.py
