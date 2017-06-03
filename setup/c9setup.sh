1f907329ce3f858af90a5fe20ae214ad7d9a5673
Harrison Bond <baronvonbadguy@gmail.com>
2017-06-03 12:03:48 -0400

    Update setup script (#3)
    * updated setup to use miniconda
    
    * updated setup to install jupyter
    
    * updated setup to install jupyter
    
    * updated setup to configure notebook correctly
    
    * fixed incorrect command

--- setup/c9setup.sh
+++ setup/c9setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-if [ -d $HOME/anaconda2 ]
+if [ -d $HOME/miniconda3 ]
 then
-    rm -rf $HOME/anaconda2
+    rm -rf $HOME/miniconda3
 fi
-wget http://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh -O Anaconda2-4.0.0-Linux-x86_64.sh
-bash Anaconda2-4.0.0-Linux-x86_64.sh -b
-export PATH="$HOME/anaconda2/bin:$PATH"
-echo 'export PATH=$HOME/anaconda2/bin:$PATH' >> ~/.bashrc
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh -b
+rm Miniconda3-latest-Linux-x86_64.sh
+export PATH="$HOME/miniconda3/bin:$PATH"
+echo 'export PATH=$HOME/miniconda3/bin:$PATH' >> ~/.bashrc
+conda install jupyter notebook --yes
+mkdir ~/.jupyter
+touch ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.token = u''" >> ~/.jupyter/jupyter_notebook_config.py
